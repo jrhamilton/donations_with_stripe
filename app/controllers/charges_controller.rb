f07a277e01df85725c52ba862ed8c40d325e6509
Jason Hamilton & John Cech <student@epicodus.com>
2014-04-23 16:44:08 -0700

    Allows user to specifiy donation amount

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,17 +6,19 @@ class ChargesController < ApplicationController
   def create
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-
   token = params[:stripeToken]
-
+  amount = params[:amount].to_f
+  amount = amount * 100
+  amount = amount.to_i
 
   begin
     charge = Stripe::Charge.create(
-      :amount => 500, # amount in cents, again
+      :amount => amount, # amount in cents, again
       :currency => "usd",
       :card => token,
       :description => "payinguser@example.com"
     )
+    @amount = charge.amount.to_f / 100
   rescue Stripe::CardError => e
     # The card has been declined
   end
